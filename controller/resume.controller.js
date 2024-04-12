27bb214485a4d9ae12aa95f33ca1b66e4ef7e7c1
vraj1763@gmail.com <vraj1763@gmail.com>
2024-04-12 22:49:37 +0530

    generate docc

--- controller/resume.controller.js
+++ controller/resume.controller.js
@@ -4,25 +4,24 @@ const mammoth = require("mammoth");
 const Resume = require("../models/resume.model");
 const fs = require("fs");
 const AWS = require("aws-sdk");
+require("dotenv").config();
 
 const s3 = new AWS.S3();
 
 exports.generateDocx = async (req, res) => {
-  exports.generateDocx = async (req, res) => {
-    try {
-      const formData = req.body;
-      console.log(formData);
-      const { filename, docxBuffer } = await generateDocxTemplate(formData);
-      res.setHeader(
-        "Content-Type",
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-      );
-      res.setHeader(
-        "Content-Disposition",
-        `attachment; filename="${filename}"`
-      );
+  try {
+    const formData = req.body;
+    console.log(formData);
+    const { filename, docxBuffer } = await generateDocxTemplate(formData);
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    );
+    res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
 
-      // Upload the file to S3
+    let docxUrl;
+    if (process.env.NODE_ENV === "production") {
+      // Upload the file to S3 if in production
       const bucketName = "resumecollection";
       const key = filename;
       const s3UploadResponse = await uploadFileToS3(
@@ -30,51 +29,47 @@ exports.generateDocx = async (req, res) => {
         bucketName,
         key
       );
-
-      const resume = new Resume(formData);
-      resume.docxUrl = s3UploadResponse.Location;
-      await resume.save();
-
-      res.send(docxBuffer);
-    } catch (error) {
-      console.error("Error generating DOCX template:", error);
-      res.status(500).json({ error: "Error generating DOCX template" });
+      docxUrl = s3UploadResponse.Location;
+    } else {
+      // Save the file locally for development
+      const filePath = `C:/Users/Vraj/Documents/resume-builder/Server/${filename}`;
+      fs.writeFileSync(filePath, docxBuffer);
+      docxUrl = `http://localhost:3000/${filename}`; // Assuming your local server runs on port 3000
     }
-  };
 
-  const uploadFileToS3 = (fileBuffer, bucketName, key) => {
-    const params = {
-      Bucket: bucketName,
-      Key: key,
-      Body: fileBuffer,
-    };
+    const resume = new Resume(formData);
+    resume.docxUrl = docxUrl;
+    await resume.save();
 
-    return s3.upload(params).promise();
-  };
+    res.send(docxBuffer);
+  } catch (error) {
+    console.error("Error generating DOCX template:", error);
+    res.status(500).json({ error: "Error generating DOCX template" });
+  }
+};
 
-  exports.convertToHtml = (req, res) => {
-    try {
-      mammoth.convertToHtml({ path: "templates.docx" }).then((result) => {
-        let html = result.value;
+exports.convertToHtml = (req, res) => {
+  try {
+    mammoth.convertToHtml({ path: "templates.docx" }).then((result) => {
+      let html = result.value;
 
-        html = html.replace(/<h\d>/g, "<hr>$&");
+      html = html.replace(/<h\d>/g, "<hr>$&");
 
-        res.send(html);
-      });
-    } catch (error) {
-      console.error("Error converting to HTML:", error);
-      res.status(500).json({ error: "Error converting document to HTML" });
-    }
-  };
+      res.send(html);
+    });
+  } catch (error) {
+    console.error("Error converting to HTML:", error);
+    res.status(500).json({ error: "Error converting document to HTML" });
+  }
+};
 
-  exports.saveEditedDocx = (req, res) => {
-    try {
-      const editedHtml = req.body.html;
-      const editedDocx = htmlToDocx(editedHtml);
-      res.send(editedDocx);
-    } catch (error) {
-      console.error("Error saving edited DOCX:", error);
-      res.status(500).json({ error: "Error saving edited DOCX" });
-    }
-  };
+exports.saveEditedDocx = (req, res) => {
+  try {
+    const editedHtml = req.body.html;
+    const editedDocx = htmlToDocx(editedHtml);
+    res.send(editedDocx);
+  } catch (error) {
+    console.error("Error saving edited DOCX:", error);
+    res.status(500).json({ error: "Error saving edited DOCX" });
+  }
 };
